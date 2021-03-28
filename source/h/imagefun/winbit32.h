79bc1fd15737492c16ab86d3834e40cd66f071b3
Dynarithmic <paulm@dynarithmic.com>
2021-03-28 16:45:53 -0400

    Update to 5.2.0.6.  See updates/updates.txt for more information.

--- source/h/imagefun/winbit32.h
+++ source/h/imagefun/winbit32.h
@@ -387,6 +387,9 @@ namespace dynarithmic
             // Normalization of irregular DIBs
             static HANDLE NormalizeDib(HANDLE hDib);
 
+            // Convert Dib to HBITMAP
+            static HBITMAP DIBToBitmap(HANDLE hDib, HPALETTE hPal = NULL);
+
             static double GetScaleFactorPerInch(LONG Unit);
 
         protected:
