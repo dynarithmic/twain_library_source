8ad59987dd73b767849caf058743228f18bcc961
Dynarithmic <paulm@dynarithmic.com>
2023-01-04 18:29:35 -0500

    Update copyright date, libraries.

--- source/h/dtwain_verinfo.h
+++ source/h/dtwain_verinfo.h
@@ -1,6 +1,6 @@
 /*
     This file is part of the Dynarithmic TWAIN Library (DTWAIN).
-    Copyright (c) 2002-2022 Dynarithmic Software.
+    Copyright (c) 2002-2023 Dynarithmic Software.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@
 
 #define DTWAIN_VERINFO_COMMENTS             "Patch Level " DTWAIN_VERINFO_PATCHLEVEL_VERSION "\0"
 #define DTWAIN_VERINFO_COMPANYNAME          "Dynarithmic Software\0"
-#define DTWAIN_VERINFO_LEGALCOPYRIGHT       "Copyright © 2020-2022\0"
+#define DTWAIN_VERINFO_LEGALCOPYRIGHT       "Copyright © 2020-2023\0"
 #define DTWAIN_VERINFO_PRODUCTNAME          "Dynarithmic Software Twain Library " UNICODE_VERSION "\0"
 #define DTWAIN_VERINFO_INTERNALNAME         DTWAIN_DLLNAME
 #define DTWAIN_VERINFO_ORIGINALFILENAME     DTWAIN_DLLNAME
