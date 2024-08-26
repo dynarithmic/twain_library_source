bfa894e401b5925500f0b46a53f01759548b9b8e
Dynarithmic <paulm@dynarithmic.com>
2024-08-26 15:33:43 -0400

    Additional minor code optimizations

--- source/h/ctliface.h
+++ source/h/ctliface.h
@@ -840,6 +840,8 @@ namespace dynarithmic
     DTWAIN_ARRAY CreateArrayFromFactory(LONG nEnumType, LONG nInitialSize);
     DTWAIN_ARRAY CreateArrayCopyFromFactory(DTWAIN_ARRAY Source);
     DTWAIN_FRAME CreateFrameArray(CTL_TwainDLLHandle* pHandle, double Left, double Top, double Right, double Bottom);
+    void SetArrayValueFromFactory(DTWAIN_ARRAY pArray, size_t lPos, LPVOID pVariant);
+
 
     typedef CTL_StringType(CTL_ITwainSource::* SOURCEINFOFUNC)() const;
     LONG GetSourceInfo(CTL_ITwainSource* p, SOURCEINFOFUNC pFunc, LPTSTR szInfo, LONG nMaxLen);
