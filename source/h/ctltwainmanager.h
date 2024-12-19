81b02adef2ad2369e39d6b55f477e5710a1c8672
Dynarithmic <paulm@dynarithmic.com>
2024-12-19 16:15:39 -0500

    Adjust TWAIN message loop depending on INI setting.

--- source/h/ctltwainmanager.h
+++ source/h/ctltwainmanager.h
@@ -61,6 +61,7 @@ namespace dynarithmic
     using SourceToXferReadyMap = std::map<std::string, SourceXferReadyOverride>;
     using SourceToXferReadyList = std::vector<std::pair<std::string, uint32_t>>;
     using SourceFlatbedOnlyList = std::unordered_set<std::string>;
+    using SourcePeekMessageList = std::unordered_set<std::string>;
 
     class CTL_TwainAppMgr;
     typedef std::shared_ptr<CTL_TwainAppMgr> CTL_TwainAppMgrPtr;
@@ -291,6 +292,7 @@ namespace dynarithmic
             static SourceToXferReadyMap& GetSourceToXferReadyMap() { return s_SourceToXferReadyMap; }
             static SourceToXferReadyList& GetSourceToXferReadyList() { return s_SourceToXferReadyList; }
             static SourceFlatbedOnlyList& GetSourceFlatbedOnlyList() { return s_SourceFlatbedOnlyList; }
+            static SourcePeekMessageList& GetSourcePeekMessageList() { return s_SourcePeekMessageList; }
             const CTL_TwainTriplet* GetCurrentTriplet() const { return m_pCurrentTriplet;}
 
         private:
@@ -503,6 +505,7 @@ namespace dynarithmic
             static SourceToXferReadyMap s_SourceToXferReadyMap;
             static SourceToXferReadyList s_SourceToXferReadyList;
             static SourceFlatbedOnlyList s_SourceFlatbedOnlyList;
+            static SourcePeekMessageList s_SourcePeekMessageList;
     };
 
     #define DTWAIN_ERROR_CONDITION(Err, RetVal, mustReport) {               \
