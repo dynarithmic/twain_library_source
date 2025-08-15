157b4d26332531a9ea3fcb29474abb5bc2cc36f2
Dynarithmic <paulm@dynarithmic.com>
2025-08-15 08:25:01 -0400

    Add experimental AES-256 bit code.

--- source/hashlib/sha3.cpp
+++ source/hashlib/sha3.cpp
@@ -154,14 +154,14 @@ void SHA3::processBlock(const void* data)
     for (unsigned int j = 0; j < StateSize; j += 5)
     {
       // temporaries
-      uint64_t one = m_hash[j];
+      uint64_t one_ = m_hash[j];
       uint64_t two = m_hash[j + 1];
 
       m_hash[j]     ^= m_hash[j + 2] & ~two;
       m_hash[j + 1] ^= m_hash[j + 3] & ~m_hash[j + 2];
       m_hash[j + 2] ^= m_hash[j + 4] & ~m_hash[j + 3];
-      m_hash[j + 3] ^=      one      & ~m_hash[j + 4];
-      m_hash[j + 4] ^=      two      & ~one;
+      m_hash[j + 3] ^=      one_      & ~m_hash[j + 4];
+      m_hash[j + 4] ^=      two      & ~one_;
     }
 
     // Iota
