c6e17b99d2b794f3adb4ab7849b87a2f16b857bd
Dynarithmic <paulm@dynarithmic.com>
2025-08-19 18:53:12 -0400

    Update code for AES-256 PDF file encryption.

---
+++ source/cpp/ctlrandnumutils.cpp
@@ -0,0 +1,41 @@
+/*
+    This file is part of the Dynarithmic TWAIN Library (DTWAIN).
+    Copyright (c) 2002-2025 Dynarithmic Software.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
+    DYNARITHMIC SOFTWARE. DYNARITHMIC SOFTWARE DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
+    OF THIRD PARTY RIGHTS.
+ */
+
+#include <ctlrandnumutils.h>
+#include <random>
+#include <vector>
+#include <algorithm>
+
+namespace dynarithmic
+{
+    std::vector<unsigned char> CreateRandomDigits(size_t num_bytes)
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distrib(0, 255);
+        std::vector<unsigned char> random_bytes(num_bytes);
+        std::generate(random_bytes.begin(), random_bytes.end(), [&]() 
+        {
+             return static_cast<unsigned char>(distrib(gen));
+        });
+        return random_bytes;
+    }
+}
