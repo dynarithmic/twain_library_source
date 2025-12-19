ad634f5528c4d31392d09eeb4147a509a072574f
Dynarithmic <paulm@dynarithmic.com>
2025-12-19 18:41:48 -0500

    Update all language bindings.

--- language_bindings_and_examples/VisualBasic/dtwain32d.vb
+++ language_bindings_and_examples/VisualBasic/dtwain32d.vb
@@ -1727,6 +1727,7 @@ Class DTWAINAPI
     Public Const DTWAIN_CONSTANT_ICAP As Integer = 78
     Public Const DTWAIN_CONSTANT_DTWAIN_CONT As Integer = 79
     Public Const DTWAIN_CONSTANT_CAPCODE_MAP As Integer = 80
+    Public Const DTWAIN_CONSTANT_ACAP As Integer = 81
     Public Const DTWAIN_USERRES_START As Integer = 20000
     Public Const DTWAIN_USERRES_MAXSIZE As Integer = 8192
     Public Const DTWAIN_APIHANDLEOK As Integer = 1
