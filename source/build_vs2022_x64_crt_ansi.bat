fbfc2b4e3b9b2a5914a2847ebef0ea78d269583f
Dynarithmic <paulm@dynarithmic.com>
2026-05-23 19:40:41 -0400

    Update CMake batch files

--- source/build_vs2022_x64_crt_ansi.bat
+++ source/build_vs2022_x64_crt_ansi.bat
@@ -10,18 +10,17 @@ rem
 rem Edit the values below if desired.
 rem ============================================================
 
-rem If using automatic Boost download (you can use any directory name other than D:/BoostDeps):
-set "BOOST_CACHE_ROOT=D:/BoostDeps"
-set "EXISTING_BOOST_ROOT="
-
-rem If using an existing Boost install, use this instead:
-rem set "EXISTING_BOOST_ROOT=D:/boost_1_90_0"
-
 set "SOURCE_DIR=%~dp0."
 for %%I in ("%SOURCE_DIR%") do set "SOURCE_DIR=%%~fI"
+
+rem If using automatic Boost download (you can use any directory name other than D:/BoostDeps):
+set "BOOST_CACHE_ROOT=%SOURCE_DIR%\BoostDeps"
+set "EXISTING_BOOST_ROOT="
+
 set "BUILD_DIR=%SOURCE_DIR%\build-vs2022-x64-crt_ansi"
 
-set BOOST_CACHE_ROOT=D:/BoostDeps
+rem If using an existing Boost install, use this instead (for example, if boost is installed in D:/boost_1_90_0):
+rem set "EXISTING_BOOST_ROOT=D:/boost_1_90_0"
 
 cmake -S "%SOURCE_DIR%" ^
       -B "%BUILD_DIR%" ^
