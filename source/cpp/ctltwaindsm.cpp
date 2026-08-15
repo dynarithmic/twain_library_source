2ee898ea36ea8ef864a20babc427b6cfe9614272
Dynarithmic <paulm@dynarithmic.com>
2026-08-14 22:34:43 -0400

    Ensure DTWAIN_SetTwainDSM() is only available when TWAIN session is not active

--- source/cpp/ctltwaindsm.cpp
+++ source/cpp/ctltwaindsm.cpp
@@ -25,6 +25,7 @@
 #include "ctlstringutils.h"
 #include "ctlstringutilsx.h"
 #include "ctltwainmanager.h"
+#include "errorcheck.h"
 using namespace dynarithmic;
 
 namespace
@@ -53,6 +54,9 @@ extern "C"
     {
         LOG_FUNC_ENTRY_PARAMS((DSMType))
         auto [pHandle, pSource] = VerifyHandles(nullptr, DTWAIN_VERIFY_DLLHANDLE);
+        BOOL sessionEnabled = pHandle->m_bSessionAllocated;
+        DTWAIN_Check_Error_Condition_WithThrow_Ex(pHandle, [&] { return sessionEnabled; }, 
+                                                  DTWAIN_ERR_ACTIVE_TWAINSESSION, nullptr, FUNC_MACRO);
         #ifndef _WIN64
         if ( DSMType == DTWAIN_TWAINDSM_LEGACY || DSMType == DTWAIN_TWAINDSM_LATESTVERSION)
         {
