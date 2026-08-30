fca54ee039dcd909178a0b1c2e27a508d371a232
Dynarithmic <paulm@dynarithmic.com>
2026-08-30 10:20:50 -0400

    Update C/C++ demo program (FullDemo)

--- demos/C_C++/C/FullDemo/dtwdemo.c
+++ demos/C_C++/C/FullDemo/dtwdemo.c
@@ -269,20 +269,11 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     /* Initialize DTWAIN */
     while (1)
     {
-        /* Try initialization, but do not show error
-           message box if there is a failure */
-        if (DTWAIN_SysInitializeNoBlocking())
-            break; 
-
-        /* Retry initialization with alternate path */
+        /* Point to where the language text resources reside */
         DTWAIN_SetResourcePathA(ALTERNATE_RESOURCE_PATH);
 
-        /* Try initialization again using the alternate path */
         if (DTWAIN_SysInitialize())
-            break;
-
-        /* Reset the resource path to the default (which is the DTWAIN DLL's path) */
-        DTWAIN_SetResourcePathA("");
+            break; 
 
         /* Failed, so either the user exits the program, or copies the 
            proper text resource files to a folder (on the path or to the 
