fa165cf6f573d02674204c73ccfddea270f5b8bc
Dynarithmic <paulm@dynarithmic.com>
2026-09-03 11:40:59 -0400

    Add C++20 CMake build scripts.

---
+++ source/build_vs2026_x32_crt_ansi_cpp20.bat
@@ -0,0 +1,16 @@
+echo Using:
+cmake --version
+echo.
+
+@echo off
+
+cmake --preset vs2026-x32-crt-ansi-cpp20
+if errorlevel 1 exit /b 1
+
+cmake --build --preset vs2026-x32-crt-ansi-cpp20-release -- /m
+if errorlevel 1 exit /b 1
+
+cmake --build --preset vs2026-x32-crt-ansi-cpp20-debug -- /m
+if errorlevel 1 exit /b 1
+
+endlocal
